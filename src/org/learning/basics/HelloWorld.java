48a12708a3eeb6fe9a5071d10632feea255d3322
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-26 13:18:32 +0100

    delete variables

--- src/org/learning/basics/HelloWorld.java
+++ src/org/learning/basics/HelloWorld.java
@@ -2,9 +2,6 @@ package org.learning.basics;
 
 public class HelloWorld {
     public static void main(String[] args){
-        int num1;
-        int num2;
-
         System.out.println("Hello world");
         System.out.println(3+8);
     }
