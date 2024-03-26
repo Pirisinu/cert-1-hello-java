f30d0edd97559c97f795608923ad958818e6795c
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-26 14:46:31 +0100

    add variables

--- src/org/learning/basics/HelloWorld.java
+++ src/org/learning/basics/HelloWorld.java
@@ -2,7 +2,18 @@ package org.learning.basics;
 
 public class HelloWorld {
     public static void main(String[] args){
+//      DECLARATION:Dichiaro una variabile
+        int num1;
+//      ASSIGNMENT:Assegno il valore
+        num1 = 10;
+//      INITIALIZATION: Declaration + Assignment
+        int num2 = 20;
+//      FINAL: il valore assegnato non può essere cambiato successivamente
+        final int result = num1 + num2;
+
+
+
         System.out.println("Hello world");
-        System.out.println(3+8);
+        System.out.println(result);
     }
 }
