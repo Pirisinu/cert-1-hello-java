28d90b5111327ad94f39156add33c464f3d00df8
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-26 14:47:27 +0100

    add array

--- src/org/learning/basics/HelloWorld.java
+++ src/org/learning/basics/HelloWorld.java
@@ -11,6 +11,11 @@ public class HelloWorld {
 //      FINAL: il valore assegnato non può essere cambiato successivamente
         final int result = num1 + num2;
 
+//      ARRAY: Collezioni ordinate di elementi
+//      non bisogna dichiarare la lunghezza se assegni direttamente i valori
+        int[] numeri = {1, 2, 3, 4, 5};
+        String[] nomi = {"Alice", "Bob", "Charlie"};
+
 
 
         System.out.println("Hello world");
