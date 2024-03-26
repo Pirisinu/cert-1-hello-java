9855d8038c22e7f3658940ba0c35986440f161d5
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-26 14:59:16 +0100

    add Class with one method

--- src/org/learning/basics/HelloWorld.java
+++ src/org/learning/basics/HelloWorld.java
@@ -16,9 +16,24 @@ public class HelloWorld {
         int[] numeri = {1, 2, 3, 4, 5};
         String[] nomi = {"Alice", "Bob", "Charlie"};
 
+//      CLASS: Una classe è un tipo di dato personalizzato definito dall’utente.
+//             Contiene attributi e metodi che descrivono le caratteristiche e il comportamento di un oggetto
+        class Persona {
+            String nome;
+            int eta;
+//          METHOD: Bisogna dichiarare
+            String saluta(){
+                return "Ciao sono " + this.nome + " ho " + this.eta + " anni";
+            }
+        }
+
+        Persona persona1 = new Persona();
+        persona1.nome = "Alice";
+        persona1.eta = 30;
 
 
         System.out.println("Hello world");
         System.out.println(result);
+        System.out.println(persona1.saluta());
     }
 }
